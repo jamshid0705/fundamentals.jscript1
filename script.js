1cd6c46cf61b89c87b5821a1cd3176abfe174195
jamshid <jamshidshamshod0705@gmail.com>
2023-02-07 23:32:45 +0500

    data typeof javacript

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let a = "javascript";
-if (a == "javascript") alert("You have found page this!");
+// let a = "javascript";
+// if (a == "javascript") alert("You have found page this!");
 
-let b = 9;
-let c = 7;
+// let b = 9;
+// let c = 7;
 
-console.log(b + c);
+// console.log(b + c);
+
+// let person1=3
+// let $function=3
+
+let script=true;
+console.log(script)
+// console.log(scriptType)
+let scriptType=false
+
+console.log(typeof 3)
+console.log(typeof true)
+console.log(typeof 'olma')
+
+let olma;
+console.log(olma)
