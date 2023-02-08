1b34b40d48a61e4c948ab2520c562a82bfc00e45
jamshid <jamshidshamshod0705@gmail.com>
2023-02-08 23:44:33 +0500

    basic opertors

--- script.js
+++ script.js
@@ -25,32 +25,60 @@
 
 /////////////// let const and var ///////////////
 
-let age;
+// let age;
 // age=34;
-// console.log(age)
+// // console.log(age)
 
-const ageboy=3;
-// ageboy=9;
-// console.log(ageboy)
+// const ageboy=3;
+// // ageboy=9;
+// // console.log(ageboy)
 
-lastName='jamshid';
-console.log(lastName)
+// lastName='jamshid';
+// console.log(lastName)
 
-// console.log(firstName)
-// let firstName;
-// firstName=2
+// // console.log(firstName)
+// // let firstName;
+// // firstName=2
 
-let jamshid
+// let jamshid
 
-console.log(jamshid)
-jamshid={
-  name:'jamshid',
-  age:22
-}
+// console.log(jamshid)
+// jamshid={
+//   name:'jamshid',
+//   age:22
+// }
 
-function func(){
-  a=9;
-  console.log(a)
-}
+// function func(){
+//   a=9;
+//   console.log(a)
+// }
 
-func()
+// func()
+
+
+///////////////////////// basic operators /////////////
+
+// math
+const ageJamshid=2023-2000
+console.log(ageJamshid/2*2, 2**5)
+
+const firstName='jamshid';
+const lastname="xatamov";
+console.log(firstName+" "+  lastname)
+
+let a=9;
+let b=9;
+let c=8
+console.log(a+b)
+c++
+c--
+// assignment operators
+console.log(a+=2)
+console.log(b-=7)
+console.log(c*=6)
+console.log(c/=2)
+
+//comparison operators
+console.log(2>8)
+console.log(lastname>firstName)
+
