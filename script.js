1dad14a2602055fbb7a61b11b3fabe2dcaaa64a8
jamshid <jamshidshamshod0705@gmail.com>
2023-02-08 23:20:10 +0500

    let const var

--- script.js
+++ script.js
@@ -9,14 +9,48 @@
 // let person1=3
 // let $function=3
 
-let script=true;
-console.log(script)
-// console.log(scriptType)
-let scriptType=false
 
-console.log(typeof 3)
-console.log(typeof true)
-console.log(typeof 'olma')
+/////////////// data type of //////////////////
+// let script=true;
+// console.log(script)
+// // console.log(scriptType)
+// let scriptType=false
 
-let olma;
-console.log(olma)
+// console.log(typeof 3)
+// console.log(typeof true)
+// console.log(typeof 'olma')
+
+// let olma;
+// console.log(olma)
+
+/////////////// let const and var ///////////////
+
+let age;
+// age=34;
+// console.log(age)
+
+const ageboy=3;
+// ageboy=9;
+// console.log(ageboy)
+
+lastName='jamshid';
+console.log(lastName)
+
+// console.log(firstName)
+// let firstName;
+// firstName=2
+
+let jamshid
+
+console.log(jamshid)
+jamshid={
+  name:'jamshid',
+  age:22
+}
+
+function func(){
+  a=9;
+  console.log(a)
+}
+
+func()
