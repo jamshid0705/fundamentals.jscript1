4d048cac951a18495a5249264469812352fecb2b
jamshid <jamshidshamshod0705@gmail.com>
2023-02-09 21:59:19 +0500

    coding challenge

--- script.js
+++ script.js
@@ -59,26 +59,71 @@
 ///////////////////////// basic operators /////////////
 
 // math
-const ageJamshid=2023-2000
-console.log(ageJamshid/2*2, 2**5)
+// const ageJamshid=2023-2000
+// console.log(ageJamshid/2*2, 2**5)
 
-const firstName='jamshid';
-const lastname="xatamov";
-console.log(firstName+" "+  lastname)
+// const firstName='jamshid';
+// const lastname="xatamov";
+// console.log(firstName+" "+  lastname)
 
-let a=9;
-let b=9;
-let c=8
-console.log(a+b)
-c++
-c--
-// assignment operators
-console.log(a+=2)
-console.log(b-=7)
-console.log(c*=6)
-console.log(c/=2)
+// let a=9;
+// let b=9;
+// let c=8
+// console.log(a+b)
+// c++
+// c--
+// // assignment operators
+// console.log(a+=2)
+// console.log(b-=7)
+// console.log(c*=6)
+// console.log(c/=2)
 
-//comparison operators
-console.log(2>8)
-console.log(lastname>firstName)
+// //comparison operators
+// console.log(2>8)
+// console.log(lastname>firstName)
 
+
+///////////////////////// operator precedence ////////////////
+ 
+//////////////////////// Coding challenge 1//////////////////
+// task 1
+
+// let markw1=78
+// let markh1=1.69
+// let johnw1=92
+// let johnh1=1.95
+
+// let BMImark1=markw1/(markh1**2)
+// let BMIjohn1=johnw1/(johnh1**2)
+// let markHinerBMI1=BMImark1>BMIjohn1
+// console.log("Mark BMI 1",BMImark1)
+// console.log("John BMI 1",BMIjohn1)
+// console.log("Mark and John",markHinerBMI1)
+
+// // task 2
+
+// let markw2 = 95;
+// let markh2 = 1.88;
+// let johnw2 = 85;
+// let johnh2 = 1.76;
+
+// let BMImark2 = markw2 / (markh2 ** 2);
+// let BMIjohn2 = johnw2 / (johnh2 ** 2);
+// let markHinerBMI2 = BMImark2 > BMIjohn2;
+// console.log("Mark BMI 2", BMImark2);
+// console.log("John BMI 2", BMIjohn2);
+// console.log("Mark and John", markHinerBMI2);
+
+
+/////////////////////// string and template ////////////
+
+const firstName='Jamshid';
+const ageYear=2000;
+const nowYear=2023;
+const job='programmer';
+
+console.log('My name is '+firstName+','+(nowYear-ageYear)+' years old '+job+'!')
+// template
+console.log(`My name is ${firstName},${nowYear-ageYear} years old ${job}! `)
+// \n 
+console.log(`My name is Jamshid.\nMy lastname Xatamov.`)
